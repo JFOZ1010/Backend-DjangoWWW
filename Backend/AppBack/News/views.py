ad62b84cb46d0abd8002f633b22f644063e9ca10
AndresGiron <andresfelipe042001@gmail.com>
2022-11-22 17:13:01 -0500

    Terminando actualizar views

--- Backend/AppBack/News/views.py
+++ Backend/AppBack/News/views.py
@@ -51,10 +51,7 @@ class allNew (generics.ListAPIView):
     permission_classes = [permissions.AllowAny]
     queryset = New.objects.all()
 
-
-
-#crear una clase para eliminar una New, y returne un http 204
-    
+#CLASE PARA BORRAR
 class DeleteNew(generics.DestroyAPIView):
     serializer_class = NewSerializer
     model = New
@@ -66,25 +63,19 @@ class DeleteNew(generics.DestroyAPIView):
         new.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+#CLASE PARA ACTUALIZAR 
 
-"""FIN BLOQUE DE CRUD DE NOTICIA
-    #funcion para actualizar un dato
-    @api_view(['POST'])
-    def updateNew(request, pk):
-        new = New.objects.get(pk=pk)
-        data = NewSerializer(instance=new, data=request.data)
+class UpdateNew (generics.UpdateAPIView):
+    serializer_class = NewSerializer
+    model = New
+    permission_classes = [permissions.AllowAny]
+    queryset = New.objects.all()
 
-        if data.is_valid():
-            data.save()
-            return Response(data.data)
-        else:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+    def put(self, request, pk, format=None):
+        new = self.get_object()
+        serializer = self.serializer_class(new, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-    #funcion para eliminar un dato
-    @api_view(['DELETE'])
-    def deleteNew(request, pk):
-        #new = New.objects.get(pk=pk)
-        new = get_object_or_404(New, pk=pk)
-        new.delete()
-        return Response(status=status.HTTP_202_ACCEPTED)"""
--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from AppBack.News.views import addNews, allNew, DeleteNew
+from AppBack.News.views import addNews, allNew, DeleteNew, UpdateNew
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('AppBack/', include('AppBack.urls')),
     path('New/create/',addNews.as_view()),
     path('New/all/',allNew.as_view()),
     path('New/delete/<str:pk>/',DeleteNew.as_view(), name="delete"),
+    path('New/update/<str:pk>/',UpdateNew.as_view(), name = "update")
 
 ]
