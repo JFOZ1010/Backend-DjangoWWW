95e1f901c4648fc425f4886daa87fd9383e5a6ef
Armando Ruiz <ArmandoRuizM@hotmail.com>
2022-12-28 23:10:29 -0500

    Se modificó para que se actualice la información de un registro duplicado solo si la fecha es diferente a la que ya se encontraba en la base de datos

--- Backend/AppBack/Api/Item/itemViews.py
+++ Backend/AppBack/Api/Item/itemViews.py
@@ -32,24 +32,41 @@ class ItemCreateApi2(APIView):
         serialized = self.serializer_class(data = request.data, many = True)
         if serialized.is_valid():
             #print(serialized)
+            #Se recorren todos los registros que se iban a insertar:
             for i in range(0, len(serialized.validated_data), 1):
-                if(len(Item.objects.filter(item_name=serialized.validated_data[i]['item_name']))!=0):
+                #Se verifica por cada registro si ya había uno en la base datos con exactamento el mismo nombre y proveedor
+                if(len(Item.objects.filter(item_name=serialized.validated_data[i]['item_name'], user_id=serialized.validated_data[i]['user_id']))!=0):
                     print("Ya estaba")
-                    #print(Item.objects.filter(item_name=serialized.validated_data[0]['item_name'])[0].item_name)
+                    #Si ya estaba, se verifica que las fechas sean diferentes 
+                    if(Item.objects.get(item_name=serialized.validated_data[i]['item_name'], user_id=serialized.validated_data[i]['user_id']).item_date!=serialized.validated_data[i]['item_date']):
+                        print("La fecha es diferente")
+                        #Si las fechas son diferentes, se actualiza el registro que ya estaba
+                        auxItem=Item.objects.get(item_name=serialized.validated_data[i]['item_name'])
+                        auxItem.item_price=serialized.validated_data[i]['item_price']
+                        auxItem.item_picture=serialized.validated_data[i]['item_picture']
+                        auxItem.item_description='Se actualizó'
+                        auxItem.item_url=serialized.validated_data[i]['item_url']
+                        auxItem.item_date=serialized.validated_data[i]['item_date']
+                        auxItem.type_id=serialized.validated_data[i]['type_id']
+                        auxItem.save()
+                        #Y se hace el guardado en el historial
+                #En caso que no se encuentre un registro con el mismo nombre, simplemente se inserta a la base de datos
                 else:
                     auxItem = Item(item_name=serialized.validated_data[i]['item_name'],
-                    item_price=serialized.validated_data[i]['item_price'],
-                    item_picture=serialized.validated_data[i]['item_picture'],
-                    item_description=serialized.validated_data[i]['item_description'],
-                    item_url=serialized.validated_data[i]['item_url'],
-                    item_date=serialized.validated_data[i]['item_date'],
-                    type_id=serialized.validated_data[i]['type_id'],
-                    user_id=serialized.validated_data[i]['user_id'])
+                                    item_price=serialized.validated_data[i]['item_price'],
+                                    item_picture=serialized.validated_data[i]['item_picture'],
+                                    item_description=serialized.validated_data[i]['item_description'],
+                                    item_url=serialized.validated_data[i]['item_url'],
+                                    item_date=serialized.validated_data[i]['item_date'],
+                                    type_id=serialized.validated_data[i]['type_id'],
+                                    user_id=serialized.validated_data[i]['user_id'])
                     auxItem.save()
             #print(len(serialized.validated_data))
             #print(serialized.validated_data[0])
             #serialized.save()
+            #Y se da una respuesta exitosa
             return Response(serialized.data, status = status.HTTP_201_CREATED)
+        #Si hubo algún error, se imprime y se da una respuesta 400
         print(serialized.errors)
         return Response(serialized.errors, status = status.HTTP_400_BAD_REQUEST)
 
