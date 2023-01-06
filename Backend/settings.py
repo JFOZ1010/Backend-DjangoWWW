c4b4da780710f6963d43048caaf8bda69b80f674
Juan Felipe Osorio <82562585+JFOZ1010@users.noreply.github.com>
2023-01-05 23:25:34 -0500

    DB - 2

--- Backend/settings.py
+++ Backend/settings.py
@@ -88,7 +88,6 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -99,14 +98,7 @@ DATABASES = {
         'DATABASE_PORT': os.getenv('DB_DATABASE_PORT'),
     }
 }
-"""
-DATABASES = {
-    'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
