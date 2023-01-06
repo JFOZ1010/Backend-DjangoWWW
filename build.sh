c961b2afa738273330da2177c6d08d420f13b7d0
Juan Felipe Osorio <82562585+JFOZ1010@users.noreply.github.com>
2023-01-05 22:12:15 -0500

    pruebas

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-#poetry install
-pip install -r requirements.txt
-
-python ./Backend/manage.py collectstatic --no-input
-python ./Backend/manage.py migrate
