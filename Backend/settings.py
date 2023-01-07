c4c347dec35af8e8090682e0fba7558d513a2886
Juan Felipe Osorio <82562585+JFOZ1010@users.noreply.github.com>
2023-01-06 22:44:36 -0500

    Cambiando Databases (Settings)

--- Backend/settings.py
+++ Backend/settings.py
@@ -88,6 +88,7 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -95,6 +96,17 @@ DATABASES = {
         'USER': 'juanfelipeoz',
         'PASSWORD': 'LBox2vyKMhOjkQ2bwtQMb60HJp8XFnIu',
         'HOST': 'dpg-cdnrnmha6gdooi7cjf50-a.oregon-postgres.render.com',
+        'DATABASE_PORT': os.getenv('DB_DATABASE_PORT'),
+    }
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
         'DATABASE_PORT': os.getenv('DB_DATABASE_PORT'),
     }
 }
